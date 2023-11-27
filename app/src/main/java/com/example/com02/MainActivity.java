179b82bf7d15cdaace5c182fc570011df6428597
SURI <77846327+shinyeong-gwak@users.noreply.github.com>
2023-11-27 10:30:50 +0900

    Merge pull request #2 from CloudCom02/shinyeong
    UI 80%

--- app/src/main/java/com/example/com02/MainActivity.java
+++ app/src/main/java/com/example/com02/MainActivity.java
@@ -5,12 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint({"HandlerLeak", "MissingInflatedId"})    // handler class의 외부 클래스 참조 유지를 방지
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // 여기서 your_layout은 현재 화면의 XML 파일 이름입니다.
+
+        Button findDeviceButton = findViewById(R.id.btn_findDevice);
+
+        findDeviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼을 클릭했을 때 실행되는 부분
+                Intent intent = new Intent(MainActivity.this, DeviceListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
